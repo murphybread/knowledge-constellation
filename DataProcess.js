5152628b86c3008e67845ccf7d1a4c73392c4fc9
Murphybread <nargene@gmail.com>
2024-12-21 23:20:03 +0900

    docs: update data processing feaure

--- DataProcess.js
+++ DataProcess.js
@@ -1,8 +1,50 @@
 import fs from "fs";
 import path from "path";
+import dotenv from "dotenv";
+dotenv.config();
 
-const FILE_NAME = "Call Number Index KR.md";
+const basePath = process.env.FILE_PATH;
+const targetFileName = process.env.FILE_NAME;
+const TARGET_DIRECTORY_NAME = process.env.TARGET_DIRECTORY_NAME;
 
-fs.readFile(FILE_NAME, "utf-8", (err, data) => {
-  console.log(data);
-});
+// console.log(fs.readdirSync(basePath));
+// console.log(fs.statSync(basePath));
+
+function findFiles(dir) {
+  let results = []; // 결과 배열
+
+  const files = fs.readdirSync(dir);
+
+  for (const file of files) {
+    console.log(file);
+    if (TARGET_DIRECTORY_NAME !== file) {
+      continue;
+    }
+    const fullPath = path.join(dir, file); // 현재 경로
+    const stat = fs.statSync(fullPath);
+
+    if (stat.isDirectory()) {
+      // 하위 디렉터리 재귀 탐색
+      results = results.concat(findFiles(fullPath));
+    } else {
+      // 파일이면 결과 배열에 추가
+      results.push(fullPath);
+    }
+  }
+  return results; // 모든 파일 경로 반환
+}
+
+const foundFilePathArray = findFiles(basePath);
+console.log(foundFilePathArray);
+
+// if (foundPath) {
+//   fs.readFile(foundPath, "utf-8", (err, data) => {
+//     if (err) {
+//       console.error("파일 읽기 오류:", err);
+//       return;
+//     }
+//     console.log("파일 내용:", data);
+//   });
+// } else {
+//   console.log("파일을 찾을 수 없습니다.");
+// }
