699a47745569db7c186ef294676f7e9cc96b0ff7
Murphybread <nargene@gmail.com>
2024-12-22 20:46:10 +0900

    feat(star): extract link pattern from class data

--- DataProcess.js
+++ DataProcess.js
@@ -7,14 +7,16 @@ const basePath = process.env.FILE_PATH;
 const targetFileName = process.env.FILE_NAME;
 const TARGET_PREFIX = process.env.TARGET_PREFIX;
 
-const PATTERENS = {
+const BOOK_NAME_PATTERN = {
   MAJOR: /^[A-Za-z]?\d00$/,
   MINOR: /^[A-Za-z]?\d[1-9]0$/,
   SUB: /^[A-Za-z]?\d[1-9]0\.\d{2}$/,
 };
 
-const STAR_PATTERENS = {
-  TAGS: /#(?!#)[^\s\n]+/g,
+const STAR_PATTERN = {
+  TAG: /#(?!#)[^\s\n]+/g,
+  LINK: /\[\[(.*?)\]\]/g,
+  BOOK: /^KR\-.*/,
 };
 
 function checkVaild(stat, fullPath) {
@@ -22,7 +24,9 @@ function checkVaild(stat, fullPath) {
   const prefix = baseName.slice(0, 2);
 
   if (stat.isDirectory()) {
-    return Object.values(PATTERENS).some((pattern) => pattern.test(baseName));
+    return Object.values(BOOK_NAME_PATTERN).some((pattern) =>
+      pattern.test(baseName)
+    );
   }
   if (stat.isFile() && prefix.toLowerCase() !== TARGET_PREFIX.toLowerCase()) {
     return false;
@@ -68,18 +72,24 @@ class Star {
 
   initialize() {
     this.#updateTags();
+    this.#updateLinks();
   }
 
   #updateTags() {
     // 부정전방탐색 ##같이 #연속으로 발생하지않는 조건 and \s\n와같은 공백 전까지 한가지 이상의 문자, 전역 탐색
-
-    const tags = this.#data.match(STAR_PATTERENS.TAGS);
-
-    this.#tag = tags;
+    const tag = this.#data.match(STAR_PATTERN.TAG);
+    this.#tag = tag;
   }
 
   #updateLinks() {
-    const pattern = /#(?!#)[^\s\n]+/g;
+    // /\[\[(.*?)\]\]/g 과 같이 [[]]안의 한가지 이상의 문자에대해 캡처그룹 생성. [[]]안에 [[]]이 선언될 수도있어서 비탐욕적이쿼드?사용
+    const link = [...this.#data.matchAll(STAR_PATTERN.LINK)].map(
+      (match) => match[1]
+    );
+
+    // /^KR\-.*/ KR-로 시작하는지 여부로 Link 패턴여부 Vaildate
+    const filteredLink = link.filter((link) => STAR_PATTERN.BOOK.test(link));
+    this.#link = filteredLink;
   }
   getStar() {
     return {
@@ -123,7 +133,7 @@ async function processFiles() {
   const FilePathArray = findFiles(basePath);
 
   const allStars = await updateJsonFromFiles(FilePathArray);
-  console.log(JSON.stringify(allStars.slice(0, 2), null, 2));
+  console.log(JSON.stringify(allStars.slice(0, 5), null, 2));
 }
 
 processFiles();
